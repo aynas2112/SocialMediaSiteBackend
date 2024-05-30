c50c736a576465923caa64bae2c786bfe2b8b49c
Akshat Sharma <93038242+akshats911@users.noreply.github.com>
2024-05-31 05:04:07 +0530

    add servers
    made a central server and a folder for authorization microservice.

---
+++ central_server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello to Social Media Site Backend");
+});
+
+
+app.listen(PORT, () => 
+    console.log(`Server up and running on port: ${PORT}`)
+);
+
+// mongoose.set('useFindAndModify', false);
